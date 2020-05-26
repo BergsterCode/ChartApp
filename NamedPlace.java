9ea4e9dac10910cfef7e533c3cedbbc8c7314467
Dennis Berg <62426951+BergsterCode@users.noreply.github.com>
2020-05-26 09:08:05 +0200

    Update NamedPlace.java

--- NamedPlace.java
+++ NamedPlace.java
@@ -1,4 +1,4 @@
-//Author: Dennis Eriksson Berg || deer7807@student.su.se
+//Author: Dennis Eriksson Berg
 public class NamedPlace extends Place {
     public NamedPlace(Position xy, String name, String category) {
         super(xy, name, category);
